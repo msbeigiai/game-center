80e5fa8c07de817aff5fbdcad93e0ff9a59086f4
msbeigiai <msbeigi83@gmail.com>
2023-10-19 10:48:25 +0300

    get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,16 +1,17 @@
-import { Badge } from '@chakra-ui/react';
+import { Badge } from "@chakra-ui/react";
 
 interface Props {
-    score: number;
+  score: number;
 }
 
-const CriticScore = ({score}: Props) => {
-    let color = score > 75 ? 'green' : score > 60 ? 'yellow' : ''
-
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
 
   return (
-    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius='4px'>{score}</Badge>
-  )
-}
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
 
-export default CriticScore
+export default CriticScore;
