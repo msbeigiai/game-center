5f8a368d3e9526a7b64defe3e13d12aed7d89636
msbeigiai <msbeigi83@gmail.com>
2023-10-23 17:49:48 +0300

    Implement infinite queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/useGames";
-import { Genre } from "./hooks/useGenres";
 import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatforms";
 
 export interface GameQuery {
   genre: Genre | null;
