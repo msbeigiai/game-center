1d048dbd234ff39aed5d4b92559c2c813ad13426
msbeigiai <msbeigi83@gmail.com>
2023-10-26 17:59:49 +0300

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,16 +4,19 @@ import APIClient, { FetchResponse } from "../services/api-client";
 import useGameQueryStore from "../store";
 import { Platform } from "./usePlatforms";
 
-const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
   name: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  slug: string;
 }
+
+const apiClient = new APIClient<Game>("/games");
 
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,7 +1,21 @@
+import { useParams } from "react-router-dom"
+import useGame from "../hooks/useGame";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
 
 const GameDetailsPage = () => {
+    const { slug } = useParams();
+    const { data: game, error, isLoading } = useGame(slug!)
+
+    if (isLoading) return <Spinner />;
+
+    if (error || !game) throw error;
+
+
     return (
-        <div>GameDetailsPage</div>
+        <>
+            <Heading>{game.name}</Heading>
+            <Text>{game.description_raw}</Text>
+        </>
     )
 }
 
